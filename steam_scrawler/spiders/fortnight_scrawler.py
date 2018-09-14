4a27cf6fdff2d5c581ec658f2d13e6e26548895a
Aperjump <wei@aperjump.me>
2018-09-13 23:06:47 -0700

    add support for selenuim

--- steam_scrawler/spiders/fortnight_scrawler.py
+++ steam_scrawler/spiders/fortnight_scrawler.py
@@ -1,8 +1,18 @@
 import scrapy
 import json
+from selenium import webdriver
+
 class FortnightCrawler(scrapy.Spider):
     name = 'Fortnight'
     tot_list = []
+
+    def __init__(self):
+        # use any browser you wish
+        self.browser = webdriver.Chrome()
+
+    def __del__(self):
+        self.browser.close()
+
     def start_requests(self):
         urls = ["https://www.microsoft.com/en-us/p/fortnite-standard-founders-pack/bxwnx8st04js?activetab=pivot:reviewstab"]
         for url in urls:
@@ -17,6 +27,7 @@ class FortnightCrawler(scrapy.Spider):
         helpful = selector.xpath("//div[@class='reviewFocusContainer']/div/p[@class='c-meta-text']/text()").extract_first()
         tuple_row = {'review_id': id, 'star': star, 'header': header, 'helpful': helpful, 'comment': text}
         return tuple_row
+
     def parse_page(self, response):
         review_section = response.xpath("//div[@class='srv_reviews']")
         tuple_list = [self.parse_function(iter) for iter
@@ -24,7 +35,8 @@ class FortnightCrawler(scrapy.Spider):
         self.tot_list += tuple_list
 
     def parse(self, response):
-
+        self.browser.get(response.url)
+        self.browser.find_element_by_xpath("//a[@id='reviewsPageNextAnchor']").click()
         self.parse_page(response)
         json_str = json.dumps(self.tot_list)
         fileObject = open('testdata.json', 'w')
